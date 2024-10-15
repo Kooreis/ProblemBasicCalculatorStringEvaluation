0d15d9871914d68cddc3c72be7a53a90b70df8b1
Kooreis <kooreis.official@gmail.com>
2024-10-15 12:15:44 -0500

    Added a calculate method that takes an expression as input and evaluates it. It uses a stack to handle operator precedence and parentheses.

--- solution.py
+++ solution.py
@@ -1,8 +1,28 @@
-class Calculator:
-    def __init__(self):
-        self.operators = {
-            '+': self.add,
-            '-': self.subtract,
-            '*': self.multiply,
-            '/': self.divide
-        }
+def calculate(self, expression):
+        stack = []
+        number = ''
+        for char in expression:
+            if char in '0123456789.':
+                number += char
+            elif char in self.operators:
+                if number:
+                    stack.append(float(number))
+                    number = ''
+                while (stack and stack[-1] in self.operators and
+                       self.precedence(char) <= self.precedence(stack[-1])):
+                    self.compute(stack)
+                stack.append(char)
+            elif char == '(':
+                stack.append(char)
+            elif char == ')':
+                if number:
+                    stack.append(float(number))
+                    number = ''
+                while stack[-1] != '(':
+                    self.compute(stack)
+                stack.pop()
+        if number:
+            stack.append(float(number))
+        while len(stack) != 1:
+            self.compute(stack)
+        return stack[0]
